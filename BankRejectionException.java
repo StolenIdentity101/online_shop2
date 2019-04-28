90d448d07ab6de670bb238fb698a676b3568305d
StolenIdentity101 <49965790+StolenIdentity101@users.noreply.github.com>
2019-04-27 21:47:05 -0500

    Update BankRejectionException.java

--- BankRejectionException.java
+++ BankRejectionException.java
@@ -1,4 +1,7 @@
 package final_project;
 
-public class BankRejectionException extends Exception {
+public class BankRejectionException extends Exception { //We need to fill in bank exception exception... 
+	public BankRejectionException(){
+		System.out.println("The Bank has rejection this input. Try to reinput. ");
+	}
 }
